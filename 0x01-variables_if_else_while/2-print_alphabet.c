081e894e5b969f9328558faf0efda225f7ba37d9
Nigel Omondi <nigelomondi62@gmail.com>
2023-02-17 18:48:16 +0300

    Print lowercase alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Prints the alphabet in lowercase
+ * @argi: the increment member
+ */
+int main(void)
 {
 	int i;
 
 	for (i = 0; i < 26; i++)
 		putchar('a' + i);
 	putchar('\n');
+
 	return (0);
 }

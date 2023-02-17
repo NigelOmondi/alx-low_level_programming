9de17d1f01bd3baacc0a352c854e550b77bebe4a
Nigel Omondi <nigelomondi62@gmail.com>
2023-02-17 20:07:34 +0300

    Eliminate e and q from the alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all letters of the alphabet except e and q
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{	
+		char c = 'a' + i;
+
+		if (c == 'e' || c == 'q')
+		{	
+			continue;
+		}
+
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

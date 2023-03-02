063abc84366369a8e50bc3191985c64498b5a71c
Nigel Omondi <nigelomondi62@gmail.com>
2023-03-02 08:33:21 +0300

    Reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 /**
- * reverse_array - a function that reverses the
+ * reverse_array - Reverses the
  * content of an array of integers
  *
  * @a: pointer to int array
@@ -7,14 +7,13 @@
  *
  * Return: nothing
  */
-
 void reverse_array(int *a, int n)
 {
 	int temp, s, e;
 
 	s = 0;
 	e = n - 1;
-	/* set value in array a in temp */ 
+	/* set value in array a in temp */
 	/* then place the last array in the first array */
 	/* then place value in temp to last array */
 	while (s < e)
